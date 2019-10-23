7c96dc73f490afacfd3a6ede615e6db1adb5a8f1
amlane <twelvexstring@gmail.com>
2019-10-23 12:06:47 -0500

    fine tuning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./API/server.js");
 
-const port = 6000;
+const port = process.env.PORT || 6000;
 
 server.listen(port, function() {
   console.log(`\n *** Service is running on localhost:${port} *** \n`);
